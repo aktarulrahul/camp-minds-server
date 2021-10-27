a7b37debf3f44ceb4645ec2cca644f407cd93919
aktarulrahul <contact.aktarul@gmail.com>
2021-10-28 02:00:31 +0600

    project init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// server run
+app.get('/', (req, res) => res.send('server runinng'));
+
+app.listen(port, () => console.log(`Running Server on port ${port}`));
